f40ef6100fab3e19c267c7e198968699f8f1b1b0
sofoniasjohn <sofoniasjohn8@gmail.com>
2026-03-25 16:03:26 +0300

    adding and configuring models,controllers and migrations from user side

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
-<?php
+    <?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CheckAdmin;
+use App\Http\Middleware\CheckUser;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'checkAdminRole' => CheckAdmin::class,
+            'checkUserRole' => CheckUser::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Admin\SizeController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\TempImageController; 
 use App\Http\Controllers\front\productController  as frontProductController;
+use App\Http\Controllers\front\AccountController;
+use App\Http\Controllers\front\OrderController;
 
 
 Route::post('/admin/login',[AuthController::class,'authenticate']);
@@ -18,21 +20,21 @@ Route::get('/get-categories',[frontProductController::class,'getCategories']);
 Route::get('/get-brands',[frontProductController::class,'getBrands']);
 Route::get('/get-products',[frontProductController::class,'getProducts']);
 Route::get('/get-product/{id}',[frontProductController::class,'getProduct']);
+Route::post('/register',[AccountController::class,'register']);
+Route::post('/login',[AccountController::class,'authenticate']);
 
-Route::group(['middleware'=>'auth:sanctum'],function(){
-    // Route::get('/categories',[CategoryController::class,'index']);
-    // Route::post('/categories',[CategoryController::class,'store']);
-    // Route::get('/categories/{id}',[CategoryController::class,'show']);
-    //  Route::put('/categories/{id}',[CategoryController::class,'update']);
-    //  Route::delete('/categories/{id}',[CategoryController::class,'destroy']);
 
+
+Route::group(['middleware'=>['auth:sanctum','checkUserRole']],function(){
+    Route::post('/save_product',[OrderController::class,'saveOrder']);
+});
+Route::group(['middleware'=>['auth:sanctum','checkAdminRole']],function(){
+  
     Route::resource('categories',CategoryController::class);
     Route::resource('brands',BrandController::class);
     Route::get('/sizes',[SizeController::class,'index']);
     Route::resource('products',ProductController::class);
     Route::put('products/setDDefaultImage/{id}',[ProductController::class,'setDefaultImage']);
     Route::delete('products/removeImage/{id}',[ProductController::class,'removeImage']);
-    // Route::post('/temp-images',[TempImageController::class,'store']);
-    // Route::delete('/temp-images/{id}',[TempImageController::class,'destroy']);
     Route::resource('temp-images',TempImageController::class);
 });
